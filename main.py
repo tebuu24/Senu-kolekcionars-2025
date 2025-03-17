bf72c0b6de2cba5f1a10bf51fa78f61e5a552007
marta-te <122258127+marta-te@users.noreply.github.com>
2025-03-17 09:17:36 +0200

    Pogu savienojumi + db
    No home screen var tikt uz login vai registraciju.
    Ir datubaze lietotajiem kurā teorētiski glabājas info par lietotājiem un no register vajadzētu sanākt pievienot tos lietotajus.
    nelielas izmainas arī .ui failu pogu nosaukumos

--- main.py
+++ main.py
@@ -11,21 +11,29 @@ class WelcomeScreen(QMainWindow):
         loadUi("welcomescreen.ui", self)
         self.widget = widget
         self.login.clicked.connect(self.gotologin)
+        self.createAccount.clicked.connect(self.gotocreate)
 
     def gotologin(self):
         login = LoginScreen(self.widget)
         self.widget.addWidget(login)
         self.widget.setCurrentIndex(self.widget.currentIndex() + 1)
+    def gotocreate(self):
+        createAccount = RegisterScreen(self.widget)
+        self.widget.addWidget(createAccount)
+        self.widget.setCurrentIndex(self.widget.currentIndex() + 1)
 
 #Login screen
 class LoginScreen(QDialog):
+    
     def __init__(self, widget):
         super(LoginScreen, self).__init__()
         loadUi("login.ui", self)
         self.widget = widget
+        self.loginbutton.clicked.connect(self.loginfunction)
+
 
     def loginfunction(self):
-        user = self.emailfield.text()
+        user = self.usernamefield.text()
         password = self.passwordfield.text()
 
         if len(user) == 0 or len(password) == 0:
@@ -33,13 +41,45 @@ class LoginScreen(QDialog):
         else:
             conn = sqlite3.connect("lietotaji.db")
             cur = conn.cursor()
-            query = "SELECT password FROM login_info WHERE username = ?"
+            query = "SELECT password FROM lietotaji WHERE username = ?"
             cur.execute(query, (user,))
             result_pass = cur.fetchone()[0]
             if result_pass == password:
                 print("veiksmīga pieslēgšanās")
             else:
                 self.error.setText("Nav pareiza parole vai lietotājvārds")
+                
+class RegisterScreen(QDialog):
+    def __init__(self, widget):
+        super(RegisterScreen, self).__init__()
+        loadUi("register.ui", self)
+        self.widget = widget
+
+    def registerFunction(self):
+        user = self.emailfield.text()
+        password = self.passwordfield.text()
+        confirmPassword = self.confirmpasswordfield.text()
+
+        if len(user) == 0 or len(password) == 0 or len(confirmPassword) == 0:
+            self.error.setText("Nedrīkst būt tukši lauki.")
+            return
+
+        if password != confirmPassword:
+            self.error.setText("Paroles nesakrīt.")
+            return
+
+        conn = sqlite3.connect("lietotaji.db")
+        cur = conn.cursor()
+        cur.execute("SELECT username FROM login_info WHERE username = ?", (user,))
+        if cur.fetchone():
+            self.error.setText("Lietotājvārds jau eksistē.")
+            conn.close()
+            return
+
+        cur.execute("INSERT INTO lietotaji (username, password) VALUES (?, ?)", (user, password))
+        conn.commit()
+        conn.close()
+        self.error.setText("Reģistrācija veiksmīga!")
 
 # konfigurācija/kas ir kas
 app = QApplication(sys.argv)
